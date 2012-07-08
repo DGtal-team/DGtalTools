398be873f9bdabe0a83ea385183759138404fa33
Kerautret <kerautre@loria.fr>
2012-07-08 14:36:42 +0200

    New meshViewer tools

---
+++ surfaceTools/meshViewer.cpp
@@ -0,0 +1,113 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+/**
+ * @file visuDistanceTransform.cpp
+ * @ingroup surfaceTools
+ * @author Bertrand Kerautret (\c kerautre@loria.fr )
+ * LORIA (CNRS, UMR 7503), University of Nancy, France
+ *
+ * @date 2012/07/08
+ *
+ * 
+ *
+ * This file is part of the DGtalTools.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <QtGui/qapplication.h>
+#include "DGtal/base/Common.h"
+
+
+#include <QtGui/qapplication.h>
+#include "DGtal/io/Display3D.h"
+#include "DGtal/io/viewers/Viewer3D.h"
+
+#include "DGtal/io/readers/OFFReader.h"
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+using namespace std;
+using namespace DGtal;
+
+
+///////////////////////////////////////////////////////////////////////////////
+namespace po = boost::program_options;
+
+int main( int argc, char** argv )
+{
+  // parse command line ----------------------------------------------
+  po::options_description general_opt("Allowed options are: ");
+  general_opt.add_options()
+    ("help,h", "display this message")
+    ("input-file,i", po::value<std::string>(), "off file (.off) " )
+    ("invertNormal,n", "threshold min to define binary shape" ); 
+  
+  bool parseOK=true;
+  po::variables_map vm;
+  try{
+    po::store(po::parse_command_line(argc, argv, general_opt), vm);  
+  }catch(const std::exception& ex){
+    parseOK=false;
+    trace.info()<< "Error checking program options: "<< ex.what()<< endl;
+  }
+  po::notify(vm);    
+  if( !parseOK || vm.count("help")||argc<=1)
+    {
+      std::cout << "Usage: " << argv[0] << " [input-file]\n"
+		<< "Display OFF mesh file by using QGLviewer"
+    << general_opt << "\n";
+      return 0;
+    }
+  
+  if(! vm.count("input-file"))
+    {
+      trace.error() << " The file name was defined" << endl;      
+      return 0;
+    }
+
+
+  
+  string inputFilename = vm["input-file"].as<std::string>();
+  
+  
+  QApplication application(argc,argv);
+  Viewer3D viewer;
+  viewer.setWindowTitle("simple Volume Viewer");
+  viewer.show();
+
+  bool invertNormal= vm.count("invertNormal");
+  
+  string extension = inputFilename.substr(inputFilename.find_last_of(".") + 1);
+  if(extension!="off" ){
+    trace.info() << "File extension not recognized: "<< extension << std::endl;
+    return 0;
+  }
+  
+  trace.info() << "Importing mesh... ";
+  
+  MeshFromPoints<Display3D::pointD3D> anImportedMesh;
+  OFFReader<Display3D::pointD3D>::importOFFFile(inputFilename, anImportedMesh, invertNormal);
+  
+  trace.info() << "[done]. "<< std::endl;
+
+  viewer << anImportedMesh;
+
+  viewer << Viewer3D::updateDisplay;
+  return application.exec();
+}
