17e54bc9b84c1fab1ffcc9c0c2c6e914dd5ea7c5
troussil <tristan.roussillon@liris.cnrs.fr>
2013-05-30 15:15:26 +0200

    convex hull of a pattern

---
+++ visualisation/patternTriangulation.cpp
@@ -0,0 +1,392 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/**
+ * @file patternTriangulation.cpp
+ * @ingroup visualisationTools
+ * @author Tristan Roussillon (\c tristan.roussillon@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2013/05/29
+ *
+ * This file is part of the DGtal library
+ */
+
+/**
+ * Description of patternTriangulation <p>
+ *
+ * Display the Delaunay triangulation of a digital pattern
+ * described by its rational slope.
+ */
+
+#include <iostream>
+#include <iterator>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <vector>
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+#include "DGtal/base/Common.h"
+#include "DGtal/helpers/StdDefs.h"
+
+#include "DGtal/arithmetic/LighterSternBrocot.h"
+#include "DGtal/arithmetic/Pattern.h"
+
+#include "DGtal/io/boards/Board2D.h"
+#include "DGtal/io/Color.h"
+
+using namespace DGtal;
+using namespace std;
+
+namespace po = boost::program_options;
+///////////////////////////////////////////////////////////////////////////////
+
+/** 
+ * A model of point functor that 
+ * maps an odd convergent to a convex hull vertex
+ *
+ * @tparam Point a model of point
+ */
+template <typename Point>
+struct OddConvexHullMap {
+
+public: 
+  typedef Point Vector; 
+
+public: 
+/** 
+ * Constructor. 
+ *
+ * @param aShift shift vector: 
+ * (1,-1) for standard pattern by default
+ */
+  OddConvexHullMap(const Vector& aShift = Vector(1,-1))
+    : myS(aShift) {}
+  
+/** 
+ * Main method 
+ *
+ * @param aP point to transform (convergent)
+ * @return the transformed point (convex hull vertex)
+ */
+  Point operator()(const Point& aP) const
+  {
+    return aP + myS; 
+  }
+
+private: 
+  Vector myS; 
+
+}; 
+
+/** 
+ * A model of point functor that 
+ * maps an odd convergent to a convex hull vertex
+ *
+ * @tparam Point a model of point
+ */
+template <typename Point>
+struct EvenConvexHullMap {
+
+public: 
+  typedef Point Vector; 
+
+public: 
+/** 
+ * Constructor. 
+ *
+ * @param aPoint last convergent
+ * @param aShift shift vector:
+ *  (1,-1) for standard pattern by default
+ */
+  EvenConvexHullMap(const Point& aPoint, const Vector& aShift = Vector(1,-1))
+    : myZn(aPoint), myS(aShift) {}
+  
+/** 
+ * Main method 
+ *
+ * @param aP point to transform (convergent)
+ * @return the transformed point (convex hull vertex)
+ */
+  Point operator()(const Point& aP) const
+  {
+    return myZn - aP + myS; 
+  }
+
+private: 
+  Point myZn; 
+  Vector myS; 
+
+}; 
+
+///////////////////////////////////////////////////////////////////////////////
+/** 
+ * Procedure that displays an edge described by 
+ * its two extremities. 
+ *
+ * @param aBoard board to display
+ * @param aP first end of the segment
+ * @param aQ second end of the segment
+ * 
+ * @tparam Board a model of 2d board
+ * @tparam Point a model of point
+ */
+template <typename Board, typename Point>
+void drawSegment(Board& aBoard, const Point& aP, const Point& aQ)
+{
+  aBoard.drawLine(aP[0], aP[1], aQ[0], aQ[1]);   
+  aBoard << aP << aQ; 
+}
+
+/** 
+ * Procedure that displays the edges of a polyline
+ * described by a range of point.
+ * 
+ * @param aBoard board to display
+ * @param itb begin iterator of the points range
+ * @param ite end iterator of the points range 
+ * @param aF a functor from a point to another point
+ * 
+ * @tparam Board a model of 2d board
+ * @tparam Iterator at least a model of forward iterator
+ * @tparam Functor a model of unary functor
+ */
+template <typename Board, typename Iterator, typename Functor>
+void drawSegments(Board& aBoard, 
+		  const Iterator& itb, const Iterator& ite, 
+		  const Functor& aF)
+{
+  Iterator it = itb; 
+  if (it != ite) 
+    {
+      Iterator prevIt = it; 
+      ++it; 
+      for ( ; it != ite; ++prevIt, ++it) 
+	{
+	  drawSegment( aBoard, aF(*prevIt), aF(*it) ); 
+	}
+    }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/** 
+ * Procedure that displays the convex hull
+ * of a given pattern.
+ * 
+ * @param aP irreductible pattern
+ * @param aD number of repetitions of the pattern
+ * 
+ * @tparam Pattern a model of pattern
+ * @tparam Integer a model of integer
+ */
+template <typename Pattern, typename Integer>
+void displayConvexHull(const Pattern& aP, const Integer& aD)
+{
+  typedef typename Pattern::Fraction Fraction; 
+  typedef typename Pattern::Quotient Quotient; 
+  typedef typename Pattern::Vector2I Vector;  
+  typedef std::vector<Vector> Convergents; 
+  typedef typename Pattern::Point2I Point;  
+  
+  Board2D aBoard;
+
+  //list of convergents
+  Convergents convergents, oddConvergents, evenConvergents;
+
+  //fill the lists
+  Fraction zn = aP.slope();
+  // aD > 1
+  if (aD > 1)
+    {
+      Fraction znm1 = zn.father(); 
+      znm1.selfDisplay(std::cout);   std::cout << std::endl;  
+      if (zn.odd())
+	oddConvergents.push_back(Vector(znm1.q(),znm1.p())); 
+      else 
+	evenConvergents.push_back(Vector(znm1.q(),znm1.p())); 
+    }
+  // aD >= 1
+  for (Quotient i = 1; i <= zn.k(); ++i)
+    {
+      Fraction zk = zn.reduced(i); 
+      zk.selfDisplay(std::cout); 
+      std::cout << " " << zn.k() - i << std::endl;
+      if (((zn.k() - i)%2) == 1 )
+	{ //odd
+	  oddConvergents.push_back(Vector(zk.q(),zk.p())); 
+	}
+      else 
+	{
+	  evenConvergents.push_back(Vector(zk.q(),zk.p())); 
+	}
+    }
+
+  //display the two main lists
+  Point Zn = Point(aD*zn.q(),aD*zn.p()); 
+  aBoard << Z2i::Domain( Point(0,0), Zn ); 
+  aBoard << SetMode( Zn.className(), "Grid" );
+  aBoard.setLineStyle(Board2D::Shape::SolidStyle); 
+  aBoard.setPenColor(DGtal::Color::Black);
+
+  OddConvexHullMap<Point> oddH;
+  drawSegments( aBoard, oddConvergents.begin(), oddConvergents.end(), oddH ); 
+
+  EvenConvexHullMap<Point> evenH(Zn);
+  drawSegments( aBoard, evenConvergents.begin(), evenConvergents.end(), evenH ); 
+
+  //four last segments
+  drawSegment( aBoard, Point(0,0), Zn ); 
+  if (evenConvergents.size() != 0)
+    {
+      drawSegment( aBoard, evenH(*evenConvergents.rbegin()), Zn ); 
+      if (oddConvergents.size() != 0)
+	{
+	  drawSegment( aBoard, Point(0,0), oddH(*oddConvergents.rbegin()) );
+	  drawSegment( aBoard, 
+		       oddH(*oddConvergents.begin()), 
+		       evenH(*evenConvergents.begin()) ); 
+	}
+      else 
+	{
+	  drawSegment( aBoard, Point(0,0), evenH(*evenConvergents.rbegin()) );
+	}
+    }
+
+  //save the file
+  aBoard.saveEPS("convexHull.eps");
+  
+}
+
+
+//proc CDT
+
+//proc FDT
+
+
+/** 
+ * Missing parameter error message.
+ * 
+ * @param param 
+ */
+void missingParam(std::string param)
+{
+  trace.error() << " Parameter: " << param << " is required...";
+  trace.info() << std::endl;
+  exit(1);
+}
+
+
+/**
+   Main function.
+
+   @param argc the number of parameters given on the line command.
+
+   @param argv an array of C-string, such that argv[0] is the name of
+   the program, argv[1] the first parameter, etc.
+*/
+int main(int argc, char **argv)
+{
+  // parse command line ----------------------------------------------
+  po::options_description general_opt("Allowed options are");
+  general_opt.add_options()
+    ("help,h", "display this message")
+    ("aparam,a",   po::value<int>(), "pattern a parameter" )
+    ("bparam,b",   po::value<int>(), "pattern b parameter" )
+    ("delta,d",   po::value<int>()->default_value(1), "number of repetitions" )
+    ("triangulation,t",   po::value<string>()->default_value("CH"), "output:\n\tClosest-point Delaunay triangulation {CDT}\n\tFarthest-point Delaunay triangulation {FDT}\n\tConvex hull {CH}" );
+
+  bool parseOK=true;
+  po::variables_map vm;
+  try{
+    po::store(po::parse_command_line(argc, argv, general_opt), vm);  
+  }catch(const std::exception& ex){
+    parseOK=false;
+    trace.info()<< "Error checking program options: "<< ex.what()<< endl;
+  }
+ 
+  po::notify(vm);    
+  if(!parseOK || vm.count("help")||argc<=1)
+    {
+      trace.info()<< "Draw the Delaunay triangulation of a pattern using DGtal library" <<std::endl 
+		  << "Basic usage: "<<std::endl
+		  << "\t" << argv[0] << " -a 5 -b 8 "<<std::endl
+		  << general_opt << "\n";
+      return 0;
+    }
+
+
+  if (!(vm.count("aparam"))) missingParam("-a");
+  if (!(vm.count("bparam"))) missingParam("-b");
+
+  typedef DGtal::int32_t Integer;
+  Integer a = vm["aparam"].as<int>();
+  Integer b = vm["bparam"].as<int>();
+  if ( (a < 0) || (b <= 0) )
+    {
+      trace.error() << " parameters a and b should be strictly positive.";
+      trace.info() << std::endl;
+      return 0;
+    }
+  if (a >= b)
+    {
+      trace.error() << " parameter a should be strictly less than b.";
+      trace.info() << std::endl;
+      return 0;
+    }
+  Integer d = vm["delta"].as<int>(); 
+  if (d <= 0)
+    {
+      trace.error() << " parameter d should be strictly positive";
+      trace.info() << std::endl;
+      return 0;
+    }
+  trace.info() << "a=" << a << ", b=" << b << ", d=" << d << std::endl; 
+
+  typedef DGtal::int32_t Quotient;
+  typedef LighterSternBrocot<Integer, Quotient, StdMapRebinder> SB;
+  typedef SB::Fraction Fraction; // the type for fractions
+  typedef Pattern<Fraction> MyPattern; // the type for patterns
+ 
+  MyPattern pattern( a, b );
+
+  string type = vm["triangulation"].as<string>();
+  if (type == "CDT")
+    {
+      trace.info() << " CDT " << std::endl; 
+    }
+  else if (type == "FDT")
+    {
+      trace.info() << " FDT " << std::endl; 
+
+    }
+  else if (type == "CH")
+    {
+      displayConvexHull(pattern, d); 
+    }
+  else 
+    {
+      trace.error() << " unknown output type. Try -h option. ";
+      trace.info() << std::endl;
+      return 0;
+    }
+
+  return 1;
+}
