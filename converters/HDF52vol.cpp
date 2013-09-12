38a3e70abf0a2b6773c59d5129a09fab91bb642b
Martial Tola <martial.tola@liris.cnrs.fr>
2013-09-12 12:22:26 +0200

    add HDF52vol

--- converters/HDF52vol.cpp
+++ converters/HDF52vol.cpp
@@ -15,7 +15,7 @@
  **/
 
 /**
- * @file raw2HDF5.cpp
+ * @file HDF52vol.cpp
  * @author Martial Tola (\c martial.tola@liris.cnrs.fr )
  * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
  *
@@ -27,8 +27,8 @@
 
 #include <iostream>
 #include <DGtal/base/Common.h>
-#include <DGtal/io/readers/RawReader.h>
-#include <DGtal/io/writers/HDF5Writer.h>
+#include <DGtal/io/readers/HDF5Reader.h>
+#include <DGtal/io/writers/VolWriter.h>
 #include <DGtal/helpers/StdDefs.h>
 #include "DGtal/io/colormaps/GrayscaleColorMap.h"
 #include <DGtal/images/Image.h>
@@ -65,11 +65,8 @@ int main(int argc, char**argv)
   po::options_description general_opt ( "Allowed options are: " );
   general_opt.add_options()
     ( "help,h", "display this message." )
-    ( "input,i", po::value<std::string>(), "Input raw file." )
-    ( "output,o", po::value<string>(),"Output HDF5 filename." )
-    ( "x,x", po::value<unsigned int>(),"x extent." )
-    ( "y,y", po::value<unsigned int >(),"y extent." )
-    ( "z,z", po::value<unsigned int>(),"z extent." );
+    ( "input,i", po::value<std::string>(), "Input HDF5 file." )
+    ( "output,o", po::value<string>(),"Output vol filename." );
   
   bool parseOK=true;
   po::variables_map vm;
@@ -83,9 +80,9 @@ int main(int argc, char**argv)
   po::notify ( vm );
   if (!parseOK || vm.count ( "help" ) ||argc<=1 )
     {
-      trace.info() << "Convert a 3D 8-bit raw file to HDF5."<<std::endl
+      trace.info() << "Convert a 3D 8-bit HDF5 file to vol."<<std::endl
                    << std::endl << "Basic usage: "<<std::endl
-                   << "\traw2HDF5 -x 128 -y 128 -z 128 --input <RawFileName> --output <HDF5OutputFileName> "<<std::endl
+                   << "\tHDF52vol --input <HDF5FileName> --output <VolOutputFileName> "<<std::endl
                    << general_opt << "\n";
       return 0;
     }
@@ -95,24 +92,18 @@ int main(int argc, char**argv)
   std::string filename = vm["input"].as<std::string>();
   if ( ! ( vm.count ( "output" ) ) ) missingParam ( "--output" );
   std::string outputFileName = vm["output"].as<std::string>();
- if ( ! ( vm.count ( "x" ) ) ) missingParam ( "--x" );
-  unsigned int x =  vm["x"].as<unsigned int>();
- if ( ! ( vm.count ( "y" ) ) ) missingParam ( "--y" );
-  unsigned int y =  vm["y"].as<unsigned int>();
- if ( ! ( vm.count ( "z" ) ) ) missingParam ( "--z" );
-  unsigned int z =  vm["z"].as<unsigned int>();
  
 
   typedef ImageContainerBySTLVector<Z3i::Domain, unsigned char>  MyImageC;
 
-  MyImageC  imageC = RawReader< MyImageC >::importRaw8 ( filename, Z3i::Vector(x,y,z) );
-  bool res =  HDF5Writer< MyImageC>::exportHDF5_3D(outputFileName, imageC, "/UInt8Array3D");
+  MyImageC imageC = HDF5Reader< MyImageC >::importHDF5_3D( filename, "/UInt8Array3D" );
+  bool res = VolWriter< MyImageC>::exportVol(outputFileName, imageC);
 
   if (res)
     return 0;
   else 
     {
-      trace.error()<< "Error while exporting the HDF5 file."<<std::endl;
+      trace.error()<< "Error while exporting the volume."<<std::endl;
       return 1;
     }
 }
