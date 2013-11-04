db8dbc3207f2b11c9d45390ed9cd8bf31d86d41f
Jeremy Levallois <jeremy.levallois@liris.cnrs.fr>
2013-11-05 00:25:27 +0100

    Modification of 3dCurvatureViewer with new version of DGtal

--- visualisation/3dCurvatureViewer.cpp
+++ visualisation/3dCurvatureViewer.cpp
@@ -66,17 +66,22 @@
 using namespace std;
 using namespace DGtal;
 
+const Color  AXIS_COLOR_RED( 200, 20, 20, 255 );
+const Color  AXIS_COLOR_GREEN( 20, 200, 20, 255 );
+const Color  AXIS_COLOR_BLUE( 20, 20, 200, 255 );
+const double AXIS_LINESIZE = 0.03;
+
 
 ///////////////////////////////////////////////////////////////////////////////
 
 void usage( int argc, char** argv )
 {
     trace.info() << "Usage: " << argv[ 0 ]
-		  << " <fileName.vol> <re> <\"mean\" || \"gaussian\" || \"princurv\">"<< std::endl;
+          << " <fileName.vol> <re> <\"mean\" || \"gaussian\" || \"princurv{1,2}\">"<< std::endl;
     trace.info() << "\t - <filename.vol> file you want to show the curvature information."<< std::endl;
     trace.info() << "\t - <re> Euclidean radius of the kernel."<< std::endl;
-    trace.info() << "\t - <\"mean\" || \"gaussian\"> show mean or Gaussian curvature on shape."<< std::endl;
-    trace.info() << "Example : "<< argv[ 0 ] << " Al.150.vol 7.39247665 \"mean\""<< std::endl;
+    trace.info() << "\t - <\"mean\" || \"gaussian\" || \"princurv{1,2}\"> show mean or Gaussian curvature on shape."<< std::endl;
+    trace.info() << "Example : "<< argv[ 0 ] << " Al.150.vol 7 \"princurv1""<< std::endl;
 }
 
 int main( int argc, char** argv )
@@ -91,16 +96,20 @@ int main( int argc, char** argv )
     double re_convolution_kernel = atof(argv[2]);
 
     std::string mode = argv[ 3 ];
-    if ( mode != "gaussian" && mode != "mean" && mode != "princurv" )
+    if ( mode != "gaussian" && mode != "mean" && mode != "princurv1" && mode != "princurv2")
     {
         usage( argc, argv );
         return 0;
     }
 
     // Construction of the shape from vol file
+    typedef Z3i::Space::RealPoint RealPoint;
     typedef ImageSelector< Z3i::Domain, bool>::Type Image;
     typedef SimpleThresholdForegroundPredicate< Image > ImagePredicate;
-    typedef Z3i::KSpace::Surfel Surfel;
+    typedef Z3i::KSpace KSpace;
+    typedef typename KSpace::SCell SCell;
+    typedef typename KSpace::Cell Cell;
+    typedef typename KSpace::Surfel Surfel;
     typedef LightImplicitDigitalSurface< Z3i::KSpace, ImagePredicate > MyLightImplicitDigitalSurface;
     typedef DigitalSurface< MyLightImplicitDigitalSurface > MyDigitalSurface;
 
@@ -110,9 +119,9 @@ int main( int argc, char** argv )
 
     Z3i::Domain domain = image.domain();
 
-    Z3i::KSpace KSpaceShape;
+    Z3i::KSpace K;
 
-    bool space_ok = KSpaceShape.init( domain.lowerBound(), domain.upperBound(), true );
+    bool space_ok = K.init( domain.lowerBound(), domain.upperBound(), true );
     if (!space_ok)
     {
       trace.error() << "Error in the Khalimsky space construction."<<std::endl;
@@ -120,8 +129,8 @@ int main( int argc, char** argv )
     }
 
     SurfelAdjacency< Z3i::KSpace::dimension > SAdj( true );
-    Surfel bel = Surfaces< Z3i::KSpace >::findABel( KSpaceShape, predicate, 100000 );
-    MyLightImplicitDigitalSurface LightImplDigSurf( KSpaceShape, predicate, SAdj, bel );
+    Surfel bel = Surfaces< Z3i::KSpace >::findABel( K, predicate, 100000 );
+    MyLightImplicitDigitalSurface LightImplDigSurf( K, predicate, SAdj, bel );
     MyDigitalSurface digSurf( LightImplDigSurf );
 
     typedef DepthFirstVisitor<MyDigitalSurface> Visitor;
@@ -137,16 +146,18 @@ int main( int argc, char** argv )
     // Integral Invariant stuff
 
     typedef FunctorOnCells< MyPointFunctor, Z3i::KSpace > MyCellFunctor;
-    MyCellFunctor functor ( pointFunctor, KSpaceShape ); // Creation of a functor on Cells, returning true if the cell is inside the shape
+    MyCellFunctor functor ( pointFunctor, K ); // Creation of a functor on Cells, returning true if the cell is inside the shape
 
     QApplication application( argc, argv );
-    Viewer3D<> viewer;
+    typedef Viewer3D<Z3i::Space, Z3i::KSpace> Viewer;
+    Viewer viewer( K );
     viewer.show();
 //    viewer << SetMode3D(image.domain().className(), "BoundingBox") << image.domain();
 
     VisitorRange range2( new Visitor( digSurf, *digSurf.begin() ) );
     SurfelConstIterator abegin2 = range2.begin();
 
+    trace.beginBlock("curvature computation");
     if( mode == "mean" || mode == "gaussian" )
     {
         typedef double Quantity;
@@ -157,7 +168,7 @@ int main( int argc, char** argv )
         {
             typedef IntegralInvariantMeanCurvatureEstimator< Z3i::KSpace, MyCellFunctor > MyIIMeanEstimator;
 
-            MyIIMeanEstimator estimator ( KSpaceShape, functor );
+            MyIIMeanEstimator estimator ( K, functor );
             estimator.init( h, re_convolution_kernel ); // Initialisation for a given Euclidean radius of the convolution kernel
             estimator.eval ( abegin, aend, resultsIterator ); // Computation
         }
@@ -165,7 +176,7 @@ int main( int argc, char** argv )
         {
             typedef IntegralInvariantGaussianCurvatureEstimator< Z3i::KSpace, MyCellFunctor > MyIIGaussianEstimator;
 
-            MyIIGaussianEstimator estimator ( KSpaceShape, functor );
+            MyIIGaussianEstimator estimator ( K, functor );
             estimator.init( h, re_convolution_kernel ); // Initialisation for a given Euclidean radius of the convolution kernel
             estimator.eval ( abegin, aend, resultsIterator ); // Computation
         }
@@ -204,38 +215,41 @@ int main( int argc, char** argv )
         typedef double Quantity;
         typedef EigenValues3D< Quantity >::Matrix33 Matrix3x3;
         typedef EigenValues3D< Quantity >::Vector3 Vector3;
-        typedef CurvatureInformation< Quantity, Matrix3x3, Vector3 > CurvInformation;
+        typedef CurvatureInformations CurvInformation;
 
         std::vector< CurvInformation > results;
         back_insert_iterator< std::vector< CurvInformation > > resultsIterator( results ); // output iterator for results of Integral Invariante curvature computation
 
         typedef IntegralInvariantGaussianCurvatureEstimator< Z3i::KSpace, MyCellFunctor > MyIIGaussianEstimator;
 
-        MyIIGaussianEstimator estimator ( KSpaceShape, functor );
-        estimator.init( h, re_convolution_kernel ); // Initialisation for a given Euclidean radius of the convolution kernel
-        estimator.evalComplete ( abegin, aend, resultsIterator ); // Computation
+        MyIIGaussianEstimator estimator ( K, functor );
+        estimator.init ( h, re_convolution_kernel ); // Initialisation for a given Euclidean radius of the convolution kernel
+        estimator.evalPrincipalCurvatures ( abegin, aend, resultsIterator ); // Computation
+
+        trace.endBlock();
+
+        trace.beginBlock("viewer");
 
         // Drawing results
-        SCellToMidPoint< Z3i::KSpace > midpoint( KSpaceShape );
         typedef  Matrix3x3::RowVector RowVector;
         typedef  Matrix3x3::ColumnVector ColumnVector;
         for ( unsigned int i = 0; i < results.size(); ++i )
         {
             CurvInformation current = results[ i ];
-            Z3i::Space::RealPoint center = midpoint( *abegin2 );
 
-
+            Cell unsignedSurfel = K.uCell( K.sKCoords(*abegin2) );
             viewer << CustomColors3D( DGtal::Color(255,255,255,255),
                                       DGtal::Color(255,255,255,255))
-                   << *abegin2;
+                   << unsignedSurfel;
 
-            ColumnVector normal = current.eigenVectors.column(0).getNormalized(); // don't show the normal
-            ColumnVector curv1 = current.eigenVectors.column(1).getNormalized();
-            ColumnVector curv2 = current.eigenVectors.column(2).getNormalized();
 
-            center[0] -= 0.4;// * normal;
-            center[1] -= 0.4;
-            center[2] -= 0.4;
+            ColumnVector normal = current.vectors.column(0).getNormalized(); // don't show the normal
+            ColumnVector curv1 = current.vectors.column(1).getNormalized();
+            ColumnVector curv2 = current.vectors.column(2).getNormalized();
+
+            SCellToMidPoint< KSpace > embedder( K );
+            double eps = 0.01;
+            RealPoint center = embedder( *abegin2 ) + eps*embedder( *abegin2 );
 
 //            viewer.addLine ( center[0] - 0.5 * normal[ 0],
 //                             center[1] - 0.5 * normal[1],
@@ -246,26 +260,42 @@ int main( int argc, char** argv )
 //                             DGtal::Color ( 0,0,0 ), 5.0 ); // don't show the normal
 
 
-	    viewer.setLineColor(DGtal::Color ( 20,200,200 ));
-            viewer.addLine ( DGtal::Z3i::RealPoint(center[0] -  0.5 * curv1[0],
-						   center[1] -  0.5 * curv1[1],
-						   center[2] -  0.5 * curv1[2]),
-                             DGtal::Z3i::RealPoint(center[0] +  0.5 * curv1[0],
-						   center[1] +  0.5 * curv1[1],
-						   center[2] +  0.5 * curv1[2]),
-			     5.0 );
-	    
-	    viewer.setLineColor(DGtal::Color ( 20,200,200 ));
-            viewer.addLine ( DGtal::Z3i::RealPoint(center[0] -  0.5 * curv2[0],
-						   center[1] -  0.5 * curv2[1],
-						   center[2] -  0.5 * curv2[2]),
-                             DGtal::Z3i::RealPoint(center[0] +  0.5 * curv2[0],
-						   center[1] +  0.5 * curv2[1],
-						   center[2] +  0.5 * curv2[2]),
-			     5.0 );
+            if( mode == "princurv1" )
+            {
+                viewer.setLineColor(AXIS_COLOR_BLUE);
+                viewer.addLine (
+                            RealPoint(
+                                center[0] -  0.5 * curv1[0],
+                                center[1] -  0.5 * curv1[1],
+                                center[2] -  0.5 * curv1[2]
+                            ),
+                            RealPoint(
+                                center[0] +  0.5 * curv1[0],
+                                center[1] +  0.5 * curv1[1],
+                                center[2] +  0.5 * curv1[2]
+                            ),
+                            AXIS_LINESIZE );
+            }
+            else
+            {
+                viewer.setLineColor(AXIS_COLOR_RED);
+                viewer.addLine (
+                            RealPoint(
+                                center[0] -  0.5 * curv2[0],
+                                center[1] -  0.5 * curv2[1],
+                                center[2] -  0.5 * curv2[2]
+                            ),
+                            RealPoint(
+                                center[0] +  0.5 * curv2[0],
+                                center[1] +  0.5 * curv2[1],
+                                center[2] +  0.5 * curv2[2]
+                            ),
+                            AXIS_LINESIZE );
+            }
 
             ++abegin2;
         }
+        trace.endBlock();
     }
 
     viewer << Viewer3D<>::updateDisplay;
