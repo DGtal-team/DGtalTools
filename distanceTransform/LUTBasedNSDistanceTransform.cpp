730a4d0b12e64fb38ef205e1f328205ed99a76ab
Kerautret <kerautre@loria.fr>
2013-12-08 22:58:16 +0100

    cleaning$ header

--- distanceTransform/LUTBasedNSDistanceTransform.cpp
+++ distanceTransform/LUTBasedNSDistanceTransform.cpp
@@ -64,8 +64,6 @@
 
 //processing
 #include "DGtal/geometry/curves/ArithmeticalDSS.h"
-#include "DGtal/geometry/curves/GreedySegmentation.h"
-#include "DGtal/geometry/curves/MaximalSegments.h"
 #include "DGtal/geometry/curves/FP.h"
 
 //boost
