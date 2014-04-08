b4bc785ce68ed81541f40dc7335e421a4de5e265
Kerautret <kerautre@loria.fr>
2014-04-08 18:19:10 +0200

    add BCC and IMM curvature scale space

--- estimators/curvatureScaleSpaceBCC.cpp
+++ estimators/curvatureScaleSpaceBCC.cpp
@@ -40,18 +40,16 @@
 
 //Grid curve
 #include "DGtal/geometry/curves/FreemanChain.h"
-#include "DGtal/geometry/curves/GridCurve.h"
 
 //Estimators
 #include "DGtal/geometry/curves/BinomialConvolver.h"
-#include "DGtal/geometry/surfaces/estimation/IntegralInvariantMeanCurvatureEstimator.h"
+
 
 // Generation of resulting image
 #include "DGtal/images/ImageContainerBySTLVector.h"
 #include "DGtal/io/writers/GenericWriter.h"
 #include "DGtal/io/colormaps/GradientColorMap.h"
 #include "DGtal/io/writers/PPMWriter.h"
-#include "DGtal/io/colormaps/HueShadeColorMap.h"
 
 
 #include <boost/program_options/options_description.hpp>
@@ -66,46 +64,19 @@
 using namespace DGtal;
 
 
-
-
-template< typename RealPoint >
-struct OptionsIntegralInvariant
-{
-  double alpha; // <! Alpha parameter for the convolution kernel. 1/3 by default
-  double radius; // <! Radius of the convolution kernel.
-  RealPoint center; // <! Center of the shape.
-  bool lambda_optimized;
-};
-
-/**
- *
- * @return Euclidean radius for the convolver of Integral Invariant estimators
- */
-template< typename ConstIteratorOnPoints, typename Point >
-unsigned int suggestedSizeIntegralInvariant( const double h,
-                                             const Point& center,
-                                             const ConstIteratorOnPoints& itb,
-                                             const ConstIteratorOnPoints& ite )
-{
-  typedef typename Point::Component TValue;
-
-  ConstIteratorOnPoints it = itb;
-  Point p( *it );
-  Point distance = p - center;
-  TValue minRadius = distance.norm();
-  ++it;
-
-  for ( ; it != ite; ++it )
-  {
-    p = *it;
-    distance = p - center;
-    if ( distance.norm() < minRadius )
-    {
-      minRadius = distance.norm();
-    }
-  }
-
-  return minRadius * h;
+void
+computeCurvatureBCC(double h, const FreemanChain<Z2i::Integer> &fc, std::vector<double> &resCurvature, 
+                    bool isClosed){
+  std::vector<Z2i::Point> vectPoints;
+  FreemanChain<Z2i::Integer>::getContourPoints( fc, vectPoints ); 
+  
+  typedef BinomialConvolver<std::vector<Z2i::Point>::const_iterator, double> MyBinomialConvolver;
+  typedef CurvatureFromBinomialConvolverFunctor< MyBinomialConvolver, double >   CurvatureBCFct;
+  BinomialConvolverEstimator< MyBinomialConvolver, CurvatureBCFct> BCCurvatureEstimator;
+  BCCurvatureEstimator.init( h, vectPoints.begin(), vectPoints.end(), isClosed );
+  resCurvature.clear();
+  resCurvature.resize(vectPoints.size());
+  BCCurvatureEstimator.eval( vectPoints.begin(), vectPoints.end(), resCurvature.begin() ); 
 }
 
 
@@ -115,6 +86,7 @@ namespace po = boost::program_options;
 
 int main( int argc, char** argv )
 {
+ 
   // parse command line ----------------------------------------------
   po::options_description general_opt("Allowed options are: ");
   general_opt.add_options()
@@ -141,10 +113,10 @@ int main( int argc, char** argv )
   po::notify(vm);    
   if(!parseOK || vm.count("help")||argc<=1 || (!(vm.count("FreemanChain"))) )
     {
-      trace.info()<< "Generate the Curvature Scale Sapce image using a binomial convolver based estimator." <<std::endl
+      trace.info()<< "Generate the Curvature Scale Space image using a binomial convolver based estimator." <<std::endl
                   << "The x axis is associated to the contour point and the y axis to the scale. The color represent the curvature values included between the cutoff values (set to 10 by default)."
                   <<std::endl << "Basic usage: "<<std::endl
-      << "\t curvatureScaleSpace -f ${DGtal}/examples/samples/contourS.fc --gridStepInit 0.001 --gridStepIncrement  0.0005 --gridStepFinal 0.05 -o cssResu.ppm "<<std::endl
+      << "\t curvatureScaleSpaceBCC -f ${DGtal}/examples/samples/contourS.fc --gridStepInit 0.001 --gridStepIncrement  0.0005 --gridStepFinal 0.05 -o cssResu.ppm "<<std::endl
       << general_opt << "\n";
       return 0;
     }
@@ -155,63 +127,40 @@ int main( int argc, char** argv )
   
   
   if(vm.count("FreemanChain")){
-    std::string fileName = vm["FreemanChain"].as<std::string>();
-
-    typedef Z2i::Space Space; 
-    typedef Space::Point Point; 
-    typedef Space::Integer Integer;  
-    typedef FreemanChain<Integer> FreemanChain; 
-    typedef std::vector< Point > Storage;
-    typedef Storage::const_iterator ConstIteratorOnPoints; 
-    typedef HueShadeColorMap<unsigned char> Hue;
-
-    std::vector< FreemanChain > vectFcs =  PointListReader< Point >:: getFreemanChainsFromFile<Integer> (fileName); 
-    
+    std::string fileName = vm["FreemanChain"].as<std::string>();    
+    std::vector< DGtal::FreemanChain<Z2i::Integer>  > vectFcs =  PointListReader< Z2i::Point >:: getFreemanChainsFromFile<Z2i::Integer> (fileName);     
     bool isClosed = vectFcs.at(0).isClosed(); 
-    std::cout << "# grid curve " << 1 << "/" << vectFcs.size() << " "
-              << ( (isClosed)?"closed":"open" ) << std::endl;
     
-    Storage vectPts; 
-    FreemanChain::getContourPoints( vectFcs.at(0), vectPts ); 
     
     // Preparing resulting image:
     unsigned int height =  (int)((h_final-h_initial)/h_increment);
-    Z2i::Domain domain (Z2i::Point(0,0), Z2i::Point(vectPts.size()-1, height-1));
+    // We add one point if the freemnchain is open.
+    Z2i::Domain domain (Z2i::Point(0,0), Z2i::Point(vectFcs.at(0).size()+(isClosed? 0: 1), height-1));
     Image2D cssImage(domain);    
     HueShadeColorMap<double>  gradCurvature (-curvatureCutOff, curvatureCutOff);
     
     trace.progressBar(0, height);
-    unsigned int nbLine=0;
-    for(double h= h_initial; h <= h_final; h=h+h_increment){
+    double h= h_initial;
+    for(double l= 0; l < height; l++ ){
       // Binomial estimator
-      trace.progressBar(nbLine, height);
-      
-      typedef BinomialConvolver<ConstIteratorOnPoints, double> MyBinomialConvolver;
-      typedef CurvatureFromBinomialConvolverFunctor< MyBinomialConvolver, double >   CurvatureBCFct;
-      BinomialConvolverEstimator< MyBinomialConvolver, CurvatureBCFct> BCCurvatureEstimator;
-      
-      BCCurvatureEstimator.init( h, vectPts.begin(), vectPts.end(), isClosed );
-      std::vector <double> curvatures( vectPts.size() ); 
-      BCCurvatureEstimator.eval( vectPts.begin(), vectPts.end(), curvatures.begin() ); 
-      
+      trace.progressBar(l, height);
+      std::vector<double> curvaturesBCC;
+      computeCurvatureBCC(h, vectFcs.at(0), curvaturesBCC, isClosed);
       // Output
       unsigned int j = 0;
-      for ( ConstIteratorOnPoints it = vectPts.begin(), it_end = vectPts.end();
+      for ( std::vector<double>::const_iterator it = curvaturesBCC.begin(), it_end = curvaturesBCC.end();
             it != it_end; ++it, ++j ) {
-        double c = curvatures[j];
+        double c = *it;
         c = c<-curvatureCutOff? -curvatureCutOff: c;
         c = c>curvatureCutOff? curvatureCutOff: c;
-        cssImage.setValue(Z2i::Point(j,nbLine), c); 
-      }
-      nbLine++;
+        cssImage.setValue(Z2i::Point(j, l), c); 
+      }      
+      h=h+h_increment;
     }
-    
     trace.progressBar(height, height);
     trace.info() <<std::endl;
-    
     DGtal::GenericWriter<Image2D, 2, double, HueShadeColorMap<double> >::exportFile(vm["output"].as<std::string>(), cssImage, gradCurvature );       
   }
- 
   return 0;
 }
 
---
+++ estimators/curvatureScaleSpaceIIM.cpp
@@ -0,0 +1,222 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+/**
+ * @file curvatureScaleSpace.cpp
+ * @ingroup estimators
+ *
+ * @author Bertrand Kerautret (\c kerautre@loria.fr )
+ * LORIA (CNRS, UMR 7503), University of Nancy, France
+ *
+ *
+ * @date 2014/04/02
+ *
+ * Output the image of curvature scale space
+ * 
+ * This file is part of the DGtal library.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+
+#include "DGtal/base/Common.h"
+
+#include "DGtal/shapes/ShapeFactory.h"
+#include "DGtal/shapes/Shapes.h"
+#include "DGtal/helpers/StdDefs.h"
+#include "DGtal/topology/helpers/Surfaces.h"
+
+//Grid curve
+#include "DGtal/geometry/curves/FreemanChain.h"
+#include "DGtal/geometry/curves/GridCurve.h"
+
+//Estimators
+#include "DGtal/geometry/surfaces/estimation/IntegralInvariantMeanCurvatureEstimator.h"
+#include "DGtal/geometry/surfaces/FunctorOnCells.h"
+
+// Generation of resulting image
+#include "DGtal/images/ImageContainerBySTLVector.h"
+#include "DGtal/io/writers/GenericWriter.h"
+#include "DGtal/io/colormaps/GradientColorMap.h"
+#include "DGtal/io/writers/PPMWriter.h"
+#include "DGtal/io/colormaps/HueShadeColorMap.h"
+
+// images
+#include "DGtal/images/ImageHelper.h"
+#include "DGtal/images/imagesSetsUtils/SimpleThresholdForegroundPredicate.h"
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+#include <vector>
+#include <string>
+#include <iomanip>
+
+
+using namespace DGtal;
+
+
+DGtal::ImageContainerBySTLVector<Z2i::Domain, unsigned char> 
+getImageFromFC(double scaleMax, const FreemanChain<Z2i::Integer> &fc, 
+               std::set<Z2i::KSpace::SCell> &boundarySCell)
+{
+  int minx, miny, maxx, maxy;
+  fc.computeBoundingBox(minx, miny, maxx, maxy);
+  minx-=scaleMax;
+  miny-=scaleMax;
+  maxx+=scaleMax;
+  maxy+=scaleMax;
+  Z2i::KSpace aKSpace;
+  aKSpace.init(Z2i::Point(minx, miny), Z2i::Point(maxx, maxy), false);
+  
+  FreemanChain<Z2i::Integer>::getContourSCell(aKSpace, fc, boundarySCell, true); 
+  std::set<Z2i::KSpace::Cell> interiorCell;
+  Surfaces<Z2i::KSpace>::uComputeInterior(aKSpace, boundarySCell, interiorCell, false);  
+  ImageContainerBySTLVector<Z2i::Domain, unsigned char> imageResult (Z2i::Domain(Z2i::Point(minx, miny), Z2i::Point(maxx, maxy))); 
+  for(std::set<Z2i::KSpace::Cell>::const_iterator it = interiorCell.begin(); it!=interiorCell.end(); it++){
+    imageResult.setValue(aKSpace.uCoords(*it), 1);
+  }
+  return imageResult; 
+}
+
+
+void
+
+computeCurvatureIIMC(double h, double scale, const ImageContainerBySTLVector<Z2i::Domain, unsigned char>  &image,
+                     const std::set<Z2i::KSpace::SCell> &boundarySCell, std::vector<double> &resCurvature)
+{
+  typedef DGtal::KhalimskySpaceND< 2, int > KSpace;
+  typedef DGtal::ImageContainerBySTLVector<Z2i::Domain, unsigned char> Image2D ; 
+  typedef KhalimskySpaceND<2, int>::Cell Cell;
+  typedef KhalimskySpaceND<2, int>::SCell SCell;
+  typedef SimpleThresholdForegroundPredicate< Image2D > ImageThreshPredicate;
+  typedef ImageToConstantFunctor< Image2D, ImageThreshPredicate > MyPointFunctor;
+  typedef FunctorOnCells< MyPointFunctor, KSpace > MyCellFunctor;
+
+  double re_convolution_kernel = scale; 
+
+  KSpace aKSpace;
+  aKSpace.init(image.domain().lowerBound(),
+               image.domain().upperBound(), false);
+  
+  
+  ImageThreshPredicate predicate = ImageThreshPredicate( image, 0 );
+  MyPointFunctor pointFunctor( image, predicate, 1 );    
+  MyCellFunctor functor ( pointFunctor, aKSpace );  
+  typedef IntegralInvariantMeanCurvatureEstimator< Z2i::KSpace, MyCellFunctor > MyIIMeanEstimator;
+  MyIIMeanEstimator estimator ( aKSpace, functor );
+
+  estimator.init(h , scale ); // Initialisation for a given Euclidean radius of the convolution kernel
+  std::back_insert_iterator< std::vector< double > > resultsIterator( resCurvature );
+  estimator.eval ( boundarySCell.begin(), boundarySCell.end(), resultsIterator ); // Computation
+
+}
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+namespace po = boost::program_options;
+
+int main( int argc, char** argv )
+{
+
+  // parse command line ----------------------------------------------
+  po::options_description general_opt("Allowed options are: ");
+  general_opt.add_options()
+    ("help,h", "display this message")
+    ("FreemanChain,f", po::value<std::string>(), "FreemanChain file name")
+    ("gridStep,s", po::value<double>()->default_value(1.0), "Grid step size")
+    ("radiusInit", po::value<double>()->default_value(1.0), "initial radius")
+    ("radiusIncrement", po::value<double>()->default_value(1.0), "radius increment")
+    ("radiusFinal", po::value<double>()->default_value(1.0), "final radius") 
+    ("output,o ", po::value<std::string>(), "set the output name ")
+    ("curvatureCutOff,c", po::value<double>()->default_value(10.0), "set the curvature limits to better display");
+  
+
+  typedef DGtal::ImageContainerBySTLVector<Z2i::Domain, unsigned char> Image2D ; 
+  typedef ImageContainerBySTLVector<Z2i::Domain, double > ImageCurvature;
+
+ 
+  bool parseOK=true;
+  po::variables_map vm;
+  try{
+    po::store(po::parse_command_line(argc, argv, general_opt), vm);  
+  }catch(const std::exception& ex){
+    parseOK=false;
+    trace.info()<< "Error checking program options: "<< ex.what()<< std::endl;
+  }
+  po::notify(vm);    
+  if(!parseOK || vm.count("help")||argc<=1 || (!(vm.count("FreemanChain"))) )
+    {
+      trace.info()<< "Generate the Curvature Scale Sapce image using a binomial convolver based estimator." <<std::endl
+                  << "The x axis is associated to the contour point and the y axis to the scale. The color represent the curvature values included between the cutoff values (set to 10 by default)."
+                  <<std::endl << "Basic usage: "<<std::endl
+      << "\t curvatureScaleSpaceIMM -f ${DGtal}/examples/samples/contourS.fc  --radiusInit 2 --radiusIncrement  0.5 --radiusFinal 100 -o cssResu.ppm "<<std::endl
+      << general_opt << "\n";
+      return 0;
+    }
+  double radius_initial = vm["radiusInit"].as<double>();
+  double radius_increment = vm["radiusIncrement"].as<double>();
+  double radius_final = vm["radiusFinal"].as<double>();
+  double curvatureCutOff = vm["curvatureCutOff"].as<double>();
+  double gridStep = vm["gridStep"].as<double>();
+  
+  if(vm.count("FreemanChain")){
+    std::string fileName = vm["FreemanChain"].as<std::string>();
+    
+    std::vector< DGtal::FreemanChain<Z2i::Integer>  > vectFcs =  PointListReader< Z2i::Point >:: getFreemanChainsFromFile<Z2i::Integer> (fileName);     
+    bool isClosed = vectFcs.at(0).isClosed(); 
+    
+    // Preparing resulting image:
+    unsigned int height =  (int)((radius_final-radius_initial)/radius_increment);
+    // We add one point since the 
+    Z2i::Domain domain (Z2i::Point(0,0), Z2i::Point(vectFcs.at(0).size()+(isClosed? 0: 1), height-1));
+    ImageCurvature cssImage(domain);    
+    HueShadeColorMap<double>  gradCurvature (-curvatureCutOff, curvatureCutOff);
+    
+    trace.progressBar(0, height);
+    double rad= radius_initial;
+    std::set<Z2i::KSpace::SCell> boundarySCell;
+    Image2D image = getImageFromFC( radius_final/gridStep, vectFcs.at(0), boundarySCell );
+    
+    for(double l= 0; l < height; l++ ){
+    
+      trace.progressBar(l, height);
+      std::vector<double> curvaturesIIM;
+      
+      computeCurvatureIIMC(gridStep, rad, image, boundarySCell,  curvaturesIIM);      
+      // Output
+      unsigned int j = 0;
+      for ( std::vector<double>::const_iterator it = curvaturesIIM.begin(), it_end = curvaturesIIM.end();
+            it != it_end; ++it, ++j ) {
+        double c = (*it)*gridStep;
+        c = c<-curvatureCutOff? -curvatureCutOff: c;
+        c = c>curvatureCutOff? curvatureCutOff: c;
+        cssImage.setValue(Z2i::Point(j, l), c); 
+      }      
+      rad=rad+radius_increment;
+    }
+    
+    trace.progressBar(height, height);
+    trace.info() <<std::endl;
+    
+    DGtal::GenericWriter<ImageCurvature, 2, double, HueShadeColorMap<double> >::exportFile(vm["output"].as<std::string>(), cssImage, gradCurvature );       
+  }
+ 
+  return 0;
+}
+
