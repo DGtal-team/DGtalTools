0e13d23e49db410e063532776b0f9c796c93de04
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2015-10-01 14:41:55 +0200

    Working version of implicit surface extractor.

--- visualisation/implicit-surface-extractor.cpp
+++ visualisation/implicit-surface-extractor.cpp
@@ -47,6 +47,8 @@
 #include "DGtal/topology/DigitalSurface.h"
 #include "DGtal/topology/helpers/Surfaces.h"
 #include "DGtal/shapes/GaussDigitizer.h"
+#include "DGtal/shapes/Mesh.h"
+#include "DGtal/shapes/implicit/ImplicitPolynomial3Shape.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -59,7 +61,7 @@ using namespace DGtal;
  * \f$ F(x,y,z,t) = f(x,y,z) - t | \nabla f(x,y,z) | \f$.
  */
 template <typename TSpace3, typename TSpace4, typename TPolynomial3>
-struct ImplicitSurface4DExtension {
+struct ImplicitSurface4DProductExtension {
   typedef TSpace3                          Space3;
   typedef TSpace4                          Space4;
   typedef TPolynomial3                     Polynomial3;
@@ -72,7 +74,7 @@ struct ImplicitSurface4DExtension {
   typedef RealPoint4                       RealPoint;
   typedef RealVector4                      RealVector;
 
-  ImplicitSurface4DExtension( Clone<Polynomial3> poly )
+  ImplicitSurface4DProductExtension( Clone<Polynomial3> poly )
     : f( poly )
   {
     fx  = derivative<0>( f );
@@ -161,6 +163,103 @@ private:
 };
 
 /**
+ * This 4D extension of a 3D implicit function transforms f(x,y,z) as:
+ * \f$ F(x,y,z,t) = f(x,y,z) - t | \nabla f(x,y,z) | \f$.
+ */
+template <typename TSpace3, typename TSpace4, typename TPolynomial3>
+struct ImplicitSurface4DExtension {
+  typedef TSpace3                          Space3;
+  typedef TSpace4                          Space4;
+  typedef TPolynomial3                     Polynomial3;
+  typedef typename Space3::RealPoint       RealPoint3;
+  typedef typename Space3::RealVector      RealVector3;
+  typedef typename Space3::Integer         Integer;
+  typedef typename RealPoint3::Coordinate  Ring;
+  typedef typename Space4::RealPoint       RealPoint4;
+  typedef typename Space4::RealVector      RealVector4;
+  typedef RealPoint4                       RealPoint;
+  typedef RealVector4                      RealVector;
+
+  ImplicitSurface4DExtension( Clone<Polynomial3> poly )
+    : f( poly )
+  {
+    fx  = derivative<0>( f );
+    fy  = derivative<1>( f );
+    fz  = derivative<2>( f );
+    fxx = derivative<0>( fx );
+    fxy = derivative<0>( fy );
+    fxz = derivative<0>( fz );
+    fyy = derivative<1>( fy );
+    fyz = derivative<1>( fz );
+    fzz = derivative<2>( fz );
+  }
+
+  /**
+     @param aPoint any point in the Euclidean space.
+     @return the value of the polynomial at \a aPoint.
+  */
+  Ring operator()(const RealPoint &aPoint) const
+  {
+    Ring x = aPoint[ 0 ];
+    Ring y = aPoint[ 1 ];
+    Ring z = aPoint[ 2 ];
+    Ring t = aPoint[ 3 ];
+    // RealVector3 grad( fx(x)(y)(z), fy(x)(y)(z), fz(x)(y)(z) );
+    return f(x)(y)(z) - t;
+  }
+
+  /**
+     @param aPoint any point in the Euclidean space.
+     @return 'true' if the polynomial value is < 0.
+  */
+  bool isInside(const RealPoint &aPoint) const
+  {
+    return this->operator()( aPoint ) <= NumberTraits<Ring>::ZERO;
+  }
+
+  /**
+     @param aPoint any point in the Euclidean space.
+     
+     @return INSIDE if the polynomial value is < 0, OUTSIDE if > 0,
+     ON otherwise.
+  */
+  Orientation orientation(const RealPoint &aPoint) const
+  {
+    Ring v = this->operator()( aPoint );
+    if ( v > NumberTraits<Ring>::ZERO )      return OUTSIDE;
+    else if ( v < NumberTraits<Ring>::ZERO ) return INSIDE;
+    else                                     return ON;
+  }
+
+  /**
+     @param aPoint any point in the Euclidean space.
+     @return the gradient vector of the polynomial at \a aPoint.
+  */
+  inline
+  RealVector gradient( const RealPoint &aPoint ) const
+  {
+    Ring x = aPoint[ 0 ];
+    Ring y = aPoint[ 1 ];
+    Ring z = aPoint[ 2 ];
+    Ring t = aPoint[ 3 ];
+    return RealVector( fx(x)(y)(z), fy(x)(y)(z), fz(x)(y)(z), -1.0 );
+  }
+
+private:
+  Polynomial3 f;
+  Polynomial3 fx;
+  Polynomial3 fy;
+  Polynomial3 fz;
+  Polynomial3 fxx;
+  Polynomial3 fxy;
+  Polynomial3 fxz;
+  Polynomial3 fyy;
+  Polynomial3 fyz;
+  Polynomial3 fzz;
+};
+
+
+/**
  * Computes the cells of the given complex that lies on the
  * boundary or inside the given bounds.
  */
@@ -247,6 +346,67 @@ analyzeSurface( CellOutputIterator itSure, CellOutputIterator itUnsure, DigitalS
     }
 }
 
+/** TODO
+ * Project the point [xp] onto some zero-level of [is] with precision [eps] (meaning '|is( xp )| < eps' at the end of the process.
+ * @param xp (returns) a point close to f=0.
+ * @param dim the dimension of the domain.
+ * @param is the implicit function.
+ * @param x0 initial point.
+ * @param eps the expected precision.
+ */
+template <typename ImplicitSurface, typename RealPoint>
+RealPoint projectNewton( const ImplicitSurface & is, 
+                         RealPoint x, 
+                         typename RealPoint::Coordinate epsilon,
+                         unsigned int max_iter )
+{
+  typedef typename RealPoint::Coordinate Scalar;
+  RealPoint gx;
+  Scalar f, g2;
+  Scalar eps2 = epsilon * epsilon;
+  while ( max_iter-- != 0 )
+    {
+      f = is( x );
+      if ( abs( f ) < epsilon ) return x;
+      gx = is.gradient( x );
+      g2 = gx.dot( gx );
+      if ( g2 < eps2 ) return x;
+      x -= (f/g2) * gx;
+    }
+  return x;
+}
+
+
+template <typename CubicalComplex4, typename ImplicitShape4, 
+          typename ImplicitDigitalShape4, typename ImplicitShape3>
+void projectComplex( std::vector< typename ImplicitShape3::RealPoint >& points,
+                     const CubicalComplex4& complex4,
+                     const ImplicitShape4& shape,
+                     const ImplicitDigitalShape4& dshape,
+                     const ImplicitShape3& shape3,
+                     double epsilon,
+                     unsigned int max_iter )
+{
+  typedef typename CubicalComplex4::Cell     Cell4;
+  typedef typename CubicalComplex4::Point    Point4;
+  typedef typename CubicalComplex4::CellMapConstIterator CellMapConstIterator;
+  typedef typename ImplicitShape4::RealPoint RealPoint4;
+  typedef typename ImplicitShape4::Ring      Ring;
+  typedef typename ImplicitShape3::RealPoint RealPoint3;
+  points.clear();
+  for ( CellMapConstIterator it = complex4.begin( 0 ), itE = complex4.end( 0 ); it != itE; ++it )
+    {
+      Cell4 cell    = it->first;
+      Point4 dp     = complex4.space().uCoords( cell );
+      RealPoint4 p  = dshape->embed( dp );
+      RealPoint3 p3 = RealPoint3( p[ 0 ], p[ 1 ], p[ 2 ] );
+      RealPoint3 q  = projectNewton( shape3, p3, epsilon, max_iter );
+      points.push_back( q );
+    }
+}
+
+    
+
 int main( int argc, char** argv )
 {
   typedef int                               Integer;
@@ -261,12 +421,14 @@ int main( int argc, char** argv )
   typedef Ring                              Scalar;
   typedef MPolynomial<3, Ring>              Polynomial3;
   typedef MPolynomialReader<3, Ring>        Polynomial3Reader;
+  typedef ImplicitPolynomial3Shape<Space3>  ImplicitShape3;
   typedef SpaceND<4,Integer>                Space4;
   typedef KhalimskySpaceND<4,Integer>       KSpace4;
   typedef KSpace4::Cell                     Cell4;
   typedef KSpace4::SCell                    SCell4;
   typedef Space4::Point                     Point4;
   typedef Space4::RealPoint                 RealPoint4;
+  typedef Space4::RealVector                RealVector4;
   typedef ImplicitSurface4DExtension<Space3,Space4,Polynomial3>
                                             ImplicitShape4;
   typedef GaussDigitizer< Space4, ImplicitShape4 > 
@@ -276,6 +438,7 @@ int main( int argc, char** argv )
   typedef CubicalComplex< KSpace4, Map4 >   CC4;
   typedef CC4::CellMapIterator              CellMapIterator;
   typedef CC4::CellMapConstIterator         CellMapConstIterator;
+  typedef CC4::Cells                        Cells4;
 
   //-------------- parse command line ----------------------------------------------
   namespace po = boost::program_options;
@@ -286,7 +449,10 @@ int main( int argc, char** argv )
     ("minAABB,a",  po::value<double>()->default_value( -10.0 ), "the min value of the AABB bounding box (domain)" )
     ("maxAABB,A",  po::value<double>()->default_value( 10.0 ), "the max value of the AABB bounding box (domain)" )
     ("gridstep,g", po::value< double >()->default_value( 1.0 ), "the gridstep that defines the digitization (often called h). " )
-    ("timestep,t", po::value< double >()->default_value( 1.0 ), "the gridstep that defines the digitization in the 4th dimension. " )
+    ("timestep,t", po::value< double >()->default_value( 0.000001 ), "the gridstep that defines the digitization in the 4th dimension (small is generally a good idea, default is 1e-6). " )
+    ("epsilon,e", po::value< double >()->default_value( 0.0000001 ), "the maximum precision relative to the implicit surface." )
+    ("max_iter,n", po::value< unsigned int >()->default_value( 500 ), "the maximum number of iteration in the Newton approximation of F=0." )
+    ("view,v", po::value< std::string >()->default_value( "Normal" ), "specifies if the surface is viewed as is (Normal) or if places close to singularities are highlighted (Singular)." )
     ;
 
   bool parseOK=true;
@@ -309,6 +475,11 @@ int main( int argc, char** argv )
 		<< general_opt << "\n";
       cerr << "Example:\n" << endl
            << "./implicit-surface-extractor -p \"-0.9*(y^2+z^2-1)^2-(x^2+y^2-1)^3\"" << endl
+           << " - whitney  : x^2-y*z^2" << endl
+           << " - 4lines   : x*y*(y-x)*(y-z*x)" << endl
+           << " - cone     : z^2-x^2-y^2" << endl
+           << " - simonU   : x^2-z*y^2+x^4+y^4" << endl
+           << " - cayley3  : 4*(x^2 + y^2 + z^2) + 16*x*y*z - 1" << endl
            << " - crixxi   : -0.9*(y^2+z^2-1)^2-(x^2+y^2-1)^3" << endl << endl;
       return 0;
     }
@@ -326,6 +497,7 @@ int main( int argc, char** argv )
       return 2;
     }
   // Creating implicit shape and storing it with smart pointer for automatic deallocation.
+  ImplicitShape3 shape3( poly );
   CountedPtr<ImplicitShape4> shape( new ImplicitShape4( poly ) ); 
 
   Ring min_x = vm[ "minAABB" ].as<double>();
@@ -337,7 +509,7 @@ int main( int argc, char** argv )
   // Creating digitized shape and storing it with smart pointer for automatic deallocation.
   CountedPtr<ImplicitDigitalShape4> dshape( new ImplicitDigitalShape4() );
   dshape->attach( *shape );
-  dshape->init( p1, p2, h );
+  dshape->init( p1, p2, RealVector4( h, h, h, t ) );
   Domain4 domain4 = dshape->getDomain();
   KSpace4 K4;
   K4.init( domain4.lowerBound(), domain4.upperBound(), true );
@@ -381,17 +553,44 @@ int main( int argc, char** argv )
   trace.info() << "- there are " << bdry.size() << " boundary cells." << endl;
   trace.endBlock();
 
+  //-------------- Compute priority function  -----------------------------------
+  trace.beginBlock( "Compute priority function. " );
+  Dimension d = complex4.dim();
+  for ( Dimension i = 0; i <= d; ++i )
+    {
+      for ( CellMapIterator it = complex4.begin( i ), itE = complex4.end( i ); it != itE; ++it )
+        {
+          Cell4 cell   = it->first;
+          Point4 dp    = K4.uCoords( cell );
+          RealPoint4 p = dshape->embed( dp );
+          Ring v       = (*shape)( p );
+          v = abs( 1000.0*v );
+          if ( v > 1000000.0 ) v = 1000000.0;
+          it->second.data &= ~CC4::VALUE;
+          it->second.data |= (uint32_t) floor( v );
+          // std::cout << " " << it->second.data;
+        }
+    }
+  trace.endBlock();
+
   //-------------- Collapse boundary -------------------------------------------
   trace.beginBlock( "Collapse boundary. " );
   typename CC4::DefaultCellMapIteratorPriority priority;
   CC4 bdry_complex4( K4 );
-  bdry_complex4.insertCells( bdry.begin(), bdry.end() );
+  for ( std::vector<Cell4>::const_iterator it = bdry.begin(), itE = bdry.end(); it != itE; ++it )
+    {
+      Cell4 cell = *it;
+      Dimension d = K4.uDim( cell );
+      CellMapConstIterator cmIt = complex4.find( d, cell );
+      bdry_complex4.insertCell( d, cell, cmIt->second );
+    }
+  //bdry_complex4.insertCells( bdry.begin(), bdry.end() );
   trace.info() << "- [before collapse] K_bdry =" << bdry_complex4 << endl;
   bdry_complex4.collapse( bdry.begin(), bdry.end(), priority, true, true, true );
   trace.info() << "- [after collapse]  K_bdry =" << bdry_complex4 << endl;
   for ( std::vector<Cell4>::const_iterator it = bdry.begin(), itE = bdry.end(); it != itE; ++it )
     {
-      Cell4 cell = *it;
+      Cell4 cell  = *it;
       Dimension d = K4.uDim( cell );
       CellMapConstIterator cmIt = bdry_complex4.find( d, cell );
       if ( cmIt != bdry_complex4.end( d ) ) {
@@ -405,10 +604,48 @@ int main( int argc, char** argv )
   trace.beginBlock( "Collapse all. " );
   std::copy( bdry.begin(), bdry.end(), std::back_inserter( inner ) );
   //typename CC4::DefaultCellMapIteratorPriority priority;
-  complex4.collapse( inner.begin(), inner.end(), priority, false, false, true );
+  complex4.collapse( inner.begin(), inner.end(), priority, true, true, true );
   trace.info() << "- K=" << complex4 << endl;
   trace.endBlock();
 
+  //-------------- Project complex onto surface --------------------------------
+  trace.beginBlock( "Project complex onto surface. " );
+  double epsilon        = vm[ "epsilon" ].as<double>();
+  unsigned int max_iter = vm[ "max_iter" ].as<unsigned int>();
+  std::vector<RealPoint3> points;
+  projectComplex( points, complex4, *shape, dshape, shape3, epsilon, max_iter );
+  trace.endBlock();
+
+  //-------------- Create Mesh -------------------------------------------
+  trace.beginBlock( "Create Mesh. " );
+  std::string view = vm[ "view" ].as<std::string>();
+  bool highlight = ( view == "Singular" );
+  Mesh<RealPoint3> mesh( true );
+  std::map<Cell4,unsigned int> indices;
+  int idx = 0;
+  for ( CellMapConstIterator it = complex4.begin( 0 ), itE = complex4.end( 0 ); it != itE; ++it, ++idx )
+    {
+      Cell4 cell = it->first;
+      indices[ cell ] = idx;
+      mesh.addVertex( points[ idx ] );
+    }
+  for ( CellMapConstIterator it = complex4.begin( 2 ), itE = complex4.end( 2 ); it != itE; ++it, ++idx )
+    {
+      Cell4 cell = it->first;
+      bool fixed = it->second.data & CC4::FIXED;
+      Cells4 bdry = complex4.cellBoundary( cell, true );
+      std::vector<unsigned int> face_idx;
+      for ( Cells4::const_iterator itC = bdry.begin(), itCE = bdry.end(); itC != itCE; ++itC )
+        {
+          if ( complex4.dim( *itC ) == 0 )
+            face_idx.push_back( indices[ *itC ] );
+        }
+      mesh.addQuadFace( face_idx[ 0 ], face_idx[ 1 ], face_idx[ 3 ], face_idx[ 2 ],
+                        ( fixed && highlight ? Color::White : Color(200,200,255) ) );
+     }
+  trace.endBlock();
+
+  //-------------- View surface -------------------------------------------
   QApplication application(argc,argv);
   Point4 low4 = K4.lowerBound();
   Point4 up4  = K4.upperBound();
@@ -418,94 +655,27 @@ int main( int argc, char** argv )
   Viewer3D<Space3,KSpace3> viewer( K3 );
   viewer.setWindowTitle("simple Volume Viewer");
   viewer.show();
-  Dimension d = complex4.dim();
-  for ( Dimension i = 0; i <= d; ++i )
-    {
-      for ( CellMapConstIterator it = complex4.begin( i ), itE = complex4.end( i ); it != itE; ++it )
-        {
-          bool fixed = it->second.data & CC4::FIXED;
-          Cell4 cell = it->first;
-          Point4 kcell = K4.uKCoords( cell ); 
-          Cell3 proj_cell = K3.uCell( Point3( kcell[ 0 ], kcell[ 1 ], kcell[ 2 ] ) );
-          viewer.setFillColor( fixed ? Color::Red : Color::White );
-          viewer << proj_cell;
-        }
-    }
+  viewer << mesh;
+  // for ( Dimension i = 0; i <= d; ++i )
+  //   {
+  //     for ( CellMapConstIterator it = complex4.begin( i ), itE = complex4.end( i ); it != itE; ++it )
+  //       {
+  //         bool fixed = it->second.data & CC4::FIXED;
+  //         Cell4 cell = it->first;
+  //         std::vector<Cell4> dummy;
+  //         std::back_insert_iterator< std::vector<Cell4> > outIt( dummy );
+  //         complex4.directCoFaces( outIt, cell );
+  //         if ( ! dummy.empty() ) continue;
+  //         Point4 kcell = K4.uKCoords( cell ); 
+  //         Cell3 proj_cell = K3.uCell( Point3( kcell[ 0 ], kcell[ 1 ], kcell[ 2 ] ) );
+  //         viewer.setFillColor( fixed ? Color::Red : Color::White );
+  //         viewer << proj_cell;
+  //       }
+  //   }
   viewer << Viewer3D<Space3,KSpace3>::updateDisplay;
   return application.exec();
 
 
-  // trace.beginBlock( "Creating Cubical Complex" );
-  // KSpace K;
-  // K.init( Point( 0,0,0 ), Point( 512,512,512 ), true );
-  // CC complex( K );
-  // Integer m = 10;
-  // std::vector<Cell> S;
-  // for ( Integer x = 0; x <= m; ++x )
-  //   for ( Integer y = 0; y <= m; ++y )
-  //     for ( Integer z = 0; z <= m; ++z )
-  //       {
-  //         Point k1 = Point( x, y, z ); 
-  //         S.push_back( K.uCell( k1 ) );
-  //         double d1 = Point::diagonal( 1 ).dot( k1 ) / sqrt( (double) KSpace::dimension );
-  //         RealPoint v1( k1[ 0 ] - d1 * k1[ 0 ], k1[ 1 ] - d1 * k1[ 1 ], k1[ 2 ] - d1 * k1[ 2 ] );
-  //         double n1 = v1.dot( v1 );
-  //         bool fixed = ( ( x == 0 ) && ( y == 0 ) && ( z == 0 ) )
-  //           || ( ( x == 0 ) && ( y == m ) && ( z == 0 ) )
-  //           || ( ( x == m ) && ( y == 0 ) && ( z == 0 ) )
-  //           || ( ( x == m ) && ( y == m ) && ( z == 0 ) )
-  //           || ( ( x == m/3 ) && ( y == 2*m/3 ) && ( z == 2*m/3 ) )
-  //           || ( ( x == 0 ) && ( y == 0 ) && ( z == m ) )
-  //           || ( ( x == 0 ) && ( y == m ) && ( z == m ) )
-  //           || ( ( x == m ) && ( y == 0 ) && ( z == m ) )
-  //           || ( ( x == m ) && ( y == m ) && ( z == m ) );
-  //         complex.insertCell( S.back(), 
-  //                             fixed ? CC::FIXED 
-  //                             : (uint32_t) floor(64.0 * sqrt( n1 ) ) // This is the priority for collapse 
-  //                             );
-  //       }
-  // //complex.close();
-  // trace.info() << "After close: " << complex << std::endl;
-  // trace.endBlock();
-
-  // // for 3D display with Viewer3D
-  // QApplication application(argc,argv);
-  // typedef Viewer3D<Space, KSpace> MyViewer;
-
-  // {
-  //   MyViewer viewer(K);
-  //   viewer.show();
-  //   typedef CC::CellMapConstIterator CellMapConstIterator;
-  //   for ( Dimension d = 0; d <= 3; ++d )
-  //     for ( CellMapConstIterator it = complex.begin( d ), itE = complex.end( d );
-  //           it != itE; ++it )
-  //       {
-  //         viewer << it->first;
-  //       }
-  //   viewer<< MyViewer::updateDisplay;
-  //   application.exec();
-  // }
-  
-  // trace.beginBlock( "Collapsing complex" );
-  // CC::DefaultCellMapIteratorPriority P;
-  // //DiagonalPriority<CC> P( complex );
-  // complex.collapse( S.begin(), S.end(), P, true, true );
-  // trace.info() << "After collapse: " << complex << std::endl;
-  // trace.endBlock();
-
-  // {
-  //   MyViewer viewer(K);
-  //   viewer.show();
-  //   typedef CC::CellMapConstIterator CellMapConstIterator;
-  //   for ( Dimension d = 0; d <= 3; ++d )
-  //     for ( CellMapConstIterator it = complex.begin( d ), itE = complex.end( d );
-  //           it != itE; ++it )
-  //       {
-  //         viewer << it->first;
-  //       }
-  //   viewer<< MyViewer::updateDisplay;
-  //   return application.exec();
-  // }
   return 0;
 }
 ///////////////////////////////////////////////////////////////////////////////
