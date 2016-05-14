3e3eb959aad955059939a34c79995fd4fc2d27a0
Kerautret <bertrand.kerautret@univ-lorraine.fr>
2016-05-14 17:43:19 +0200

    fix typo

--- volumetric/volSubSample.cpp
+++ volumetric/volSubSample.cpp
@@ -1,4 +1,4 @@
-vol/**
+/**
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as
  *  published by the Free Software Foundation, either version 3 of the
@@ -81,7 +81,7 @@ meshViewer -i lobster.off lobster2.off  lobster4.off  lobster8.off -n
 @endcode
 
  You should obtain such a result:
- @image html resVolSubSample.png "Resulting visualization."
+ @image html  "Resulting visualization."
  
  @see
  @ref volSubSample.cpp
