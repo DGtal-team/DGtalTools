52e0d987c70c18d6cc1cc938043a15329fe22b90
Kerautret <bertrand.kerautret@univ-lorraine.fr>
2017-03-12 10:24:21 +0100

    corrections sliceViewer

--- visualisation/sliceViewer.cpp
+++ visualisation/sliceViewer.cpp
@@ -71,10 +71,9 @@ namespace po = boost::program_options;
    @code
    -h [ --help ]                     display this message
    -i [ --input ] arg                vol file (.vol, .longvol .p3d, .pgm3d and if 
-                                     WITH_ITK is selected: dicom, dcm, mha, mhd) or 
-                                     sdp (sequence of discrete points). For longvol, 
-                                     dicom, dcm, mha or mhd formats, the input values
-                                     are linearly scaled between 0 and 255.
+                                     WITH_ITK is selected: dicom, dcm, mha, mhd). 
+                                     For longvol, dicom, dcm, mha or mhd formats, the
+                                     input values are linearly scaled between 0 and 255.
    --hueColorMap                     use hue color map to display images.
    --gradHotColorMap                 use hot gradient color map to display images.
    --gradCoolColorMap                use cool gradient color map to display images.
@@ -424,7 +423,7 @@ int main( int argc, char** argv )
   po::options_description general_opt("Allowed options are: ");
   general_opt.add_options()
     ("help,h", "display this message")
-    ("input,i", po::value<std::string>(), "vol file (.vol, .longvol .p3d, .pgm3d and if WITH_ITK is selected: dicom, dcm, mha, mhd) or sdp (sequence of discrete points). For longvol, dicom, dcm, mha or mhd formats, the input values are linearly scaled between 0 and 255." )
+    ("input,i", po::value<std::string>(), "vol file (.vol, .longvol .p3d, .pgm3d and if WITH_ITK is selected: dicom, dcm, mha, mhd). For longvol, dicom, dcm, mha or mhd formats, the input values are linearly scaled between 0 and 255." )
     ("hueColorMap", "use hue color map to display images." )
     ("gradHotColorMap", "use hot gradient color map to display images." )
     ("gradCoolColorMap", "use cool gradient color map to display images." )
