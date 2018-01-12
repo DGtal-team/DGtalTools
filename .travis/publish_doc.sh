cf4d314f89a2d9460aa29e0671c94fec3ff834f9
Kerautret <bertrand.kerautret@univ-lorraine.fr>
2018-01-12 14:34:48 +0100

    udpate

--- .travis/publish_doc.sh
+++ .travis/publish_doc.sh
@@ -4,6 +4,6 @@ set -ev
 
 cd build/
 echo $HOME
-rsync -azv --delete --delete-after -e 'ssh -oStrictHostKeyChecking=no -i  /home/travis/build/DGtal-team/DGtal/.travis/dgtal_rsa' html/ dgtal@liris.cnrs.fr:/home/dgtal/public_html/doc/tools/
+rsync -azv --delete --delete-after -e 'ssh -oStrictHostKeyChecking=no -i  /home/travis/build/DGtal-team/DGtalTools/.travis/dgtal_rsa' html/ dgtal@liris.cnrs.fr:/home/dgtal/public_html/doc/tools/nightly
 
 
