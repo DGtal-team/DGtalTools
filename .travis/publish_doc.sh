6279a8b57c1c6589d1a9ef03d37353279b00d55d
Bertrand Kerautret <bertrand.kerautret@univ-lorraine.fr>
2018-03-01 22:46:22 +0100

    Update publish_doc.sh

--- .travis/publish_doc.sh
+++ .travis/publish_doc.sh
@@ -4,6 +4,6 @@ set -ev
 
 cd build/
 echo $HOME
-rsync -azv --delete --delete-after -e 'ssh -oStrictHostKeyChecking=no -i  /home/travis/build/DGtal-team/DGtalTools/.travis/dgtal_rsa' html/ dgtal@liris.cnrs.fr:/home/dgtal/public_html/doc/tools/nightly
+rsync -azv --delete --delete-after -e 'ssh -oStrictHostKeyChecking=no -i  /home/travis/build/DGtal-team/DGtalTools/.travis/dgtal_rsa' html/ dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/tools/nightly
 
 
