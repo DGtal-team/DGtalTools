01e2275f7787d27259565d7ca0a114da5b6aef96
Kacper Pluta <kacper.pluta@bsdlabs.com.br>
2019-04-03 11:41:53 +0300

    adjust to the API changes in DGtal 1.0

--- geometricTransform/rigidTransform2D.cpp
+++ geometricTransform/rigidTransform2D.cpp
@@ -115,8 +115,8 @@ int main(int argc, char**argv)
   double ty =  vm["ty"].as<double>();
 
   typedef ImageSelector<Domain, unsigned char >::Type Image;
-  typedef ForwardRigidTransformation2D < Space, VectorRounding < RealPoint, Point>, RealPoint, Point >  ForwardTrans;
-  typedef BackwardRigidTransformation2D < Space, VectorRounding < RealPoint, Point>, RealPoint, Point > BackwardTrans;
+  typedef ForwardRigidTransformation2D < Space >  ForwardTrans;
+  typedef BackwardRigidTransformation2D < Space > BackwardTrans;
   typedef ConstImageAdapter<Image, Domain, BackwardTrans, Image::Value, Identity > MyImageBackwardAdapter;
   typedef DomainRigidTransformation2D < Domain, ForwardTrans > MyTransformedDomain;
   typedef MyTransformedDomain::Bounds Bounds;
