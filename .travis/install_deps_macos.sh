3aee8f436d5cb70af3db6235556ac3908491aa07
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-09-24 09:35:48 +0200

    Travis bot -> Xenial

--- .travis/install_deps_macos.sh
+++ .travis/install_deps_macos.sh
@@ -9,7 +9,7 @@ brew install qt5 graphicsmagick fftw eigen
 # Explicit install of libqglviewer
 # Explicit install of libqglviewer
 brew tap DGtal-team/homebrew-dgtal
-brew install libqglviewer.rb
+brew install libqglviewer
 
 ## Temporary HDF5 build issue
 export BTYPE="$BTYPE -DWITH_HDF5=false" && echo "Disabling HDF5 on MacOS";
