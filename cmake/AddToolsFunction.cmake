41a14cc7db7b8a8d4c9f14b3fc62ecda30b288b8
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2021-08-10 23:51:28 +0200

    add whitelist

--- cmake/AddToolsFunction.cmake
+++ cmake/AddToolsFunction.cmake
@@ -1,12 +1,12 @@
 # DGTALTOOLS_RANDOMIZED_BUILD_THRESHOLD must be a global variable between 0 and 99.
-# DGTALTOOLS_RANDOMIZED_BUILD_WHITELIST must be a global variable containing the set of tools that will be always build.
-
+# DGTALTOOLS_RANDOMIZED_BUILD_WHITELIST must be a global variable containing the list of tools that will be always build.
+  
 function(DGtalTools_add_tool tool_file) #optional_avoid_add_test
   string(RANDOM LENGTH 2 ALPHABET "0123456789" _random)
-  if (${_random} LESS ${DGTALTOOLS_RANDOMIZED_BUILD_THRESHOLD}  OR ${tool_file} IN_LIST DGTALTOOLS_RANDOMIZED_BUILD_WHITELIST)
+  if ((${_random} LESS ${DGTALTOOLS_RANDOMIZED_BUILD_THRESHOLD}) OR ${tool_file} IN_LIST DGTALTOOLS_RANDOMIZED_BUILD_WHITELIST )
     add_executable(${tool_file} ${tool_file}.cpp ${ARGN})
     target_link_libraries (${tool_file} ${DGTAL_LIBRARIES} ${DGtalToolsLibDependencies})
-     install(TARGETS ${tool_file}
+    install(TARGETS ${tool_file}
         RUNTIME DESTINATION bin
         LIBRARY DESTINATION lib
         ARCHIVE DESTINATION lib)
