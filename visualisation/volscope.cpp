d9b6693c7fc1cba0a81d7b45b173599cb9616565
David Coeurjolly <david.coeurjolly@cnrs.fr>
2024-01-27 10:39:30 +0100

    Update visualisation/volscope.cpp
    Co-authored-by: Bertrand Kerautret <bertrand.kerautret@univ-lyon2.fr>

--- visualisation/volscope.cpp
+++ visualisation/volscope.cpp
@@ -68,7 +68,7 @@ typedef Shortcuts<Z3i::KSpace> SH3;
  --volumetricMode                      Activate the volumetric mode instead of the isosurface visualization.
  --point-cloud-only                    In the volumetric mode, visualize the vol file as a point cloud instead of an hex mesh (default: false)
 -m,  --min INT                             For isosurface visualization and voxel filtering, specifies the threshold min (excluded) (default: 0).
- --max INT                             For isosurface visualization and voxel filtering, specifies the threshold max (included) (default: 255).
+ -M, --max INT                             For isosurface visualization and voxel filtering, specifies the threshold max (included) (default: 255).
  @endcode
 **/
 
