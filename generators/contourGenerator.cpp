3d7d99d4509f7d03f9080c0fac23af739b298175
kerautret <bertrand.kerautret@univ-lyon2.fr>
2025-07-08 14:44:14 +0200

    fix CLI11 v2.5 in generators

--- generators/contourGenerator.cpp
+++ generators/contourGenerator.cpp
@@ -476,16 +476,16 @@ int main( int argc, char** argv )
   auto radiusOpt = app.add_option("--radius,-R", radius, "Radius of the shape" );
   auto axis1Opt = app.add_option("--axis1,-A", axis1, "Half big axis of the shape (ellipse)" );
   auto axis2Opt = app.add_option("--axis2,-a", axis2, "Half small axis of the shape (ellipse)" );
-  auto smallradiusOpt = app.add_option("--smallradius,-r", smallradius, "Small radius of the shape (default 5)", true);
-  auto varsmallradiusOpt = app.add_option("--varsmallradius,-v", varsmallradius, "Variable small radius of the shape (default 5)", true );
-  auto kOpt = app.add_option("-k", k, "Number of branches or corners the shape (default 3)", true );
-  auto phiOpt = app.add_option("--phi", phi, "Phase of the shape (in radian, default 0.0)", true );
-  auto widthOpt = app.add_option("--width,-w", width, "Width of the shape (default 10.0)", true );
-  auto powerOpt = app.add_option("--power,-p", power, "Power of the metric (default 2.0)", true );
-  app.add_option("--center_x,-x", cx, "x-coordinate of the shape center (default 0.0)", true );
-  app.add_option("--center_y,-y", cy, "y-coordinate of the shape center (default 0.0)", true );
-  app.add_option("--gridstep,-g", h, "Gridstep for the digitization (default 1.0)", true );
-  auto outputFormatOpt = app.add_option("--format,-f", outputFormat, "Output format:\n\t  List of pointel coordinates {pts}\n\t  Freeman chaincode Vector {fc} (default pts)", true );
+  auto smallradiusOpt = app.add_option("--smallradius,-r", smallradius, "Small radius of the shape (default 5)");
+  auto varsmallradiusOpt = app.add_option("--varsmallradius,-v", varsmallradius, "Variable small radius of the shape (default 5)" );
+  auto kOpt = app.add_option("-k", k, "Number of branches or corners the shape (default 3)" );
+  auto phiOpt = app.add_option("--phi", phi, "Phase of the shape (in radian, default 0.0)" );
+  auto widthOpt = app.add_option("--width,-w", width, "Width of the shape (default 10.0)" );
+  auto powerOpt = app.add_option("--power,-p", power, "Power of the metric (default 2.0)" );
+  app.add_option("--center_x,-x", cx, "x-coordinate of the shape center (default 0.0)" );
+  app.add_option("--center_y,-y", cy, "y-coordinate of the shape center (default 0.0)" );
+  app.add_option("--gridstep,-g", h, "Gridstep for the digitization (default 1.0)" );
+  auto outputFormatOpt = app.add_option("--format,-f", outputFormat, "Output format:\n\t  List of pointel coordinates {pts}\n\t  Freeman chaincode Vector {fc} (default pts)");
   auto outputFileNameOpt = app.add_option("--outputGeometry,-o", outputFileName, "Base name of the file containing the shape geometry (points, tangents, curvature)" );
   
   app.get_formatter()->column_width(40);
