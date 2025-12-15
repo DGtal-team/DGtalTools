5befe3074d606c445d70189050ebd40e92efc027
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2025-12-15 15:07:31 +0100

    Release 2.1

--- cmake/AddToolsFunction.cmake
+++ cmake/AddToolsFunction.cmake
@@ -5,7 +5,7 @@ function(DGtalTools_add_tool tool_file) #optional_avoid_add_test
   string(RANDOM LENGTH 2 ALPHABET "0123456789" _random)
   if ((${_random} LESS ${DGTALTOOLS_RANDOMIZED_BUILD_THRESHOLD}) OR ${tool_file} IN_LIST DGTALTOOLS_RANDOMIZED_BUILD_WHITELIST )
     add_executable(${tool_file} ${tool_file}.cpp ${ARGN})
-    target_link_libraries (${tool_file} ${DGTAL_LIBRARIES} ${DGtalLibDependencies} ${DGtalToolsLibDependencies})
+    target_link_libraries (${tool_file} DGtal::DGtal ${DGtalToolsLibDependencies})
     install(TARGETS ${tool_file}
         RUNTIME DESTINATION bin
         LIBRARY DESTINATION lib
